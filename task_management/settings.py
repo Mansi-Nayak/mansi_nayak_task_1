ec46fb88946b7f1d3700aaa4ea44fa2fe9e99e1d
Mansi Nayak <nayak.mansi09@gmail.com>
2025-05-02 11:43:38 +0530

    update the settings file

--- task_management/settings.py
+++ task_management/settings.py
@@ -139,7 +139,8 @@ AUTH_USER_MODEL = "app.User"
 
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+# EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
